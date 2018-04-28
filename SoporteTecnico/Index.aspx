68dbccb6281eb1dbc63c8c992c72a4787a08a371
Luis Samano <martindjt@hotmail.com>
2018-04-27 19:54:06 -0500

    Pantilla Basica, Bootstrap 4.0.0 y JQuery 3.0.0

---
+++ SoporteTecnico/Index.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="SoporteTecnico.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+    
+    <title>Inicio</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!--Bootstap CSS--->
+    <link rel="stylesheet" href="Content/bootstrap.css" />
+
+    <asp:ContentPlaceHolder ID="StyleSection" runat="server" />
+
+</head>
+<body>
+    <header>
+
+    </header>
+    <nav>
+
+    </nav>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+
+
+    <!--Scripts Section--->
+    <script src="Scripts/jquery.-3.0.0.js" ></script>
+    <script src="Scripts/bootstrap.js" ></script>
+    <asp:ContentPlaceHolder ID="ScriptSection" runat="server" />
+
+</body>
+</html>
---
+++ SoporteTecnico/Index.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SoporteTecnico {
+    
+    
+    public partial class Index {
+        
+        /// <summary>
+        /// Control StyleSection.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder StyleSection;
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control ScriptSection.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ScriptSection;
+    }
+}
